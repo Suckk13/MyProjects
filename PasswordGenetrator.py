a44390f47f288f1bfb4f0cf9d681fbefcc154564
realcode.cc <128353710+Suckk13@users.noreply.github.com>
2023-09-09 21:33:27 +0800

    Password-Generator by realcode.cc
    NEW MULTIFUNCTIONAL PASSWORD  GENERTOR ON PYTHON!  BY  realcode.cc
    **************************************************************************************
    YOU CAN SAVE ONLY ONE PASSWORD!!!

---
+++ PasswordGenetrator.py
@@ -0,0 +1,40 @@
+#password_generator by realcode.cc
+#файлы для работы и настройки, НЕ ТРОГАТЬ ВСЕ СЛОМАЕТСЯ
+import random
+file = open('Passwords.txt', 'w+')
+file.read()
+saving_password = []
+feedback = """
+            Help - справка
+            Add  - Добавить пароль
+            Show - показать список паролей"""
+print(feedback)
+programm_is_run = True
+#Основной код
+while programm_is_run:
+    command = input('Введите команду\n')
+    command = command.lower()
+
+    #help command
+    if command == 'help':
+        print(feedback)
+
+    #show command
+    elif command == 'show':
+        print(saving_password)
+    #add  command
+    elif command == 'add':
+        password = ''
+        lenght = int(input('Введите длинну пароля: '))
+
+        for x in range(lenght):
+            password += random.choice(('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'))
+        print('Сгенерированный пароль: ', password)
+        app = input('Для какого приложения вы хотите его использовать? ')
+        complete = password, ':', app
+        saving_password.append(complete)
+        forfile = str(saving_password)
+        file.write(forfile)
+        file.close()
+    else:
+        print('Команда не найдена')
